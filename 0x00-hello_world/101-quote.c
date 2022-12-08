74a8c94e51b65f37413a74157ce5fb268e26e1d1
Sultanakinola <sultanakinola9@gmail.com>
2022-12-08 14:27:38 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,8 @@
 #include <stdio>
 #include <unistd.h>
+
 /**
- * main - Entry
+ * main - print exactly "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19",
  *   followed by a new line, to the standard error.
  * Return: Always 0 (Success)
