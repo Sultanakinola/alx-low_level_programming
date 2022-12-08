c67b0e6c57d0d4ce0bca7966d4d7bf8004cb7d34
Sultanakinola <sultanakinola9@gmail.com>
2022-12-08 07:54:12 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-include <stdio.h>
+#include <stdio.h>
 /**
- * * main - Entry point
- *  *
- *   * Return: Always 0 (Success)
- *    */
+ * main - a line of code using puts
+ *
+ * Return: 0
+ */
 int main(void)
 {
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
