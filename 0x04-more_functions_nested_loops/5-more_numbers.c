728786ea4bb555866a5fce5dbe8056cd768f47f3
Sultanakinola <sultanakinola9@gmail.com>
2022-12-15 04:46:10 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,13 +13,13 @@ void more_numbers(void)
 	int counter;
 
 	for (counter = 1; counter <= 10; counter++)
-		 {
-			 for (i = 0; i <= 14; i++)
-			 {
-				 if (i / 10 > 0)
-					 _putchar((i / 10) + '0');
-				 _putchar((i % 10) + '0');
-			 }
-			 _putchar('\n');
-		 }
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i / 10 > 0)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
