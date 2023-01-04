8fe8edcc6024a836be60603143b57e71359efeac
Sultanakinola <sultanakinola9@gmail.com>
2023-01-04 15:43:28 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,43 +1,31 @@
 #include "main.h"
-/**
- * is_square_root - Entry point
- * Description - A function that confirms the natural
- * square root of an input
- * *@num: the function accepts an input saved into num
- * *@squared: the function accepts an input saved into squared
- * Return: Success (integer)
- */
-int is_square_root(int num, int squared)
-{
-	if ((squared * squared) == num)
-	{
-		return (squared);
-	}
-	else if (squared == num / 2)
-	{
-		return (-1);
-	}
-	return (is_square_root(num, squared + 1));
-}
 
+/**
+ * check- check for square root 
+ * @a: int 
+ * @b: int 
+ *
+ * Return: int
+ */
+int check(int a, int b)
+{
+	if (a*a == b)
+		return(a);
+	if (a*a > b)
+		return (-1)
+			return (check(a+1,b));
+}
 
 /**
  * _sqrt_recursion - Entry point
  * Description - A function that returns the
  * natural square root of a number
- * *@n: the function accepts an input saved into n
- * Return: Success (integer)
+ * @n: the function accepts an input saved into n
+ * Return: natural square root or -1
  */
 int _sqrt_recursion(int n)
 {
-	int squared = 0;
-	if (n < 0)
-	{
-		return (-1);
-	}
-	else if (n == 1)
-	{
-		return (1);
-	}
-	return (is_square_root(n, squared));
+	if (n == 0)
+		return (0);
+	return (check(1,n));
 }
