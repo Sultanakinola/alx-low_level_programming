3abeb87c100b85a906c00ee8ed180466e425bef1
sultanakinola <sultanakinola9@gmail.com>
2023-01-19 23:21:32 -0800

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,15 +1,18 @@
 #include "3-calc.h"
-#include "3-calc.h"
+#include <stddef.h>
 
 /**
- * get_op_func - selects the correct function to perform
- * the operation asked by the user.
- * @s: char operator.
+ * get_op_func - get ops function pointer of type char array
+ * that accepts two inputs of int data type
  *
- * Return: pointer to the function that corresponds to the operator.
+ * @s: a character pointer pointing to a symbol from the program argument
+ *
+ * Return: one of the operator functions to perform calculations
  */
+
 int (*get_op_func(char *s))(int, int)
 {
+	/* struct opts of struct op_t */
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
@@ -19,11 +22,13 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}
 	};
 	int i = 0;
-	while (i < 10)
+
+	while (i < 5)
 	{
-		if (s[0] == ops->op[i])
-			break;
+		if (*s == *ops[i].op)
+			return (ops[i].f);
 		i++;
 	}
-	return (ops[i / 2].f);
+
+	return (NULL);
 }
